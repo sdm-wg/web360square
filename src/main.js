20750aaedee6e940b8942e4116d9994212df14aa
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-06-04 17:00:28 +0900

    feat: Add event listeners for transition

--- src/main.js
+++ src/main.js
@@ -14,4 +14,36 @@ async function setup() {
 document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener(MouseEventWrapper.START, setup, false);
   load();
+
+  const front   = document.getElementById('front-page');
+  const concert = document.getElementById('concert');
+  const pops    = document.getElementById('pops');
+
+  document.querySelector('a[href="#concert"]').addEventListener(MouseEventWrapper.CLICK, (event) => {
+    // `onpopstate` が発火しないようにする
+    event.preventDefault();
+
+    front.classList.add('-hidden');
+    pops.classList.add('-hidden');
+    concert.classList.remove('-hidden');
+
+    history.pushState(null, null, '/#/concert');
+  }, false);
+
+  document.querySelector('a[href="#pops"]').addEventListener(MouseEventWrapper.CLICK, (event) => {
+    // `onpopstate` が発火しないようにする
+    event.preventDefault();
+
+    front.classList.add('-hidden');
+    concert.classList.add('-hidden');
+    pops.classList.remove('-hidden');
+
+    history.pushState(null, null, '/#/pops');
+  }, false);
+
+  window.addEventListener('popstate', () => {
+    concert.classList.add('-hidden');
+    pops.classList.add('-hidden');
+    front.classList.remove('-hidden');
+  });
 }, true);
